08420561d45905cdb0c43ce62e2981934a863969
Matt Bretl <mpbretl@gmail.com>
2019-03-21 17:11:37 +0000

    consistent hook fn style

--- src/Postgis_GeometryCollection_GeometriesPlugin.ts
+++ src/Postgis_GeometryCollection_GeometriesPlugin.ts
@@ -6,44 +6,41 @@ import {
 } from "./constants";
 
 const plugin: Plugin = builder => {
-  builder.hook(
-    "GraphQLObjectType:fields",
-    function AddGeometriesToGeometryCollection(fields, build, context) {
-      const {
-        scope: { isPgGISGeographyType, pgGISType, pgGISSubtype },
-      } = context;
-      if (!isPgGISGeographyType || pgGISSubtype !== 7) {
-        return fields;
-      }
-      const {
-        extend,
-        pgGISGraphQLInterfaceTypesByType,
-        graphql: { GraphQLList },
-      } = build;
-      const Interface = pgGISGraphQLInterfaceTypesByType[pgGISType.id];
-      if (!Interface) {
-        debug("Unexpectedly couldn't find the interface");
-        return fields;
-      }
-
-      return extend(fields, {
-        geometries: {
-          type: new GraphQLList(Interface),
-          resolve(data: any) {
-            return data.__geojson.geometries.map((geom: any) => {
-              const subtype = SUBTYPE_BY_GEOJSON_TYPE[geom.type];
-              const pgGeometryType = Object.keys(
-                SUBTYPE_BY_PG_GEOMETRY_TYPE
-              ).find(k => SUBTYPE_BY_PG_GEOMETRY_TYPE[k] === subtype);
-              return {
-                __gisType: pgGeometryType,
-                __geojson: geom,
-              };
-            });
-          },
-        },
-      });
+  builder.hook("GraphQLObjectType:fields", (fields, build, context) => {
+    const {
+      scope: { isPgGISGeographyType, pgGISType, pgGISSubtype },
+    } = context;
+    if (!isPgGISGeographyType || pgGISSubtype !== 7) {
+      return fields;
     }
-  );
+    const {
+      extend,
+      pgGISGraphQLInterfaceTypesByType,
+      graphql: { GraphQLList },
+    } = build;
+    const Interface = pgGISGraphQLInterfaceTypesByType[pgGISType.id];
+    if (!Interface) {
+      debug("Unexpectedly couldn't find the interface");
+      return fields;
+    }
+
+    return extend(fields, {
+      geometries: {
+        type: new GraphQLList(Interface),
+        resolve(data: any) {
+          return data.__geojson.geometries.map((geom: any) => {
+            const subtype = SUBTYPE_BY_GEOJSON_TYPE[geom.type];
+            const pgGeometryType = Object.keys(
+              SUBTYPE_BY_PG_GEOMETRY_TYPE
+            ).find(k => SUBTYPE_BY_PG_GEOMETRY_TYPE[k] === subtype);
+            return {
+              __gisType: pgGeometryType,
+              __geojson: geom,
+            };
+          });
+        },
+      },
+    });
+  });
 };
 export default plugin;
--- src/Postgis_Point_LatitudeLongitudePlugin.ts
+++ src/Postgis_Point_LatitudeLongitudePlugin.ts
@@ -1,43 +1,40 @@
 import { Plugin } from "graphile-build";
 
 const plugin: Plugin = builder => {
-  builder.hook(
-    "GraphQLObjectType:fields",
-    function AddLatitudeAndLongitudeToPointType(fields, build, context) {
-      const { inflection } = build;
-      const {
-        scope: { isPgGISGeographyType, pgGISType, pgGISSubtypeDetails },
-      } = context;
-      if (!isPgGISGeographyType || pgGISSubtypeDetails.subtype !== 1) {
-        return fields;
-      }
-      const {
-        extend,
-        graphql: { GraphQLNonNull, GraphQLFloat },
-      } = build;
-      const xFieldName = inflection.gisXFieldName(
-        pgGISType,
-        pgGISSubtypeDetails.srid
-      );
-      const yFieldName = inflection.gisYFieldName(
-        pgGISType,
-        pgGISSubtypeDetails.srid
-      );
-      return extend(fields, {
-        [xFieldName]: {
-          type: new GraphQLNonNull(GraphQLFloat),
-          resolve(data: any) {
-            return data.__geojson.coordinates[0];
-          },
-        },
-        [yFieldName]: {
-          type: new GraphQLNonNull(GraphQLFloat),
-          resolve(data: any) {
-            return data.__geojson.coordinates[1];
-          },
-        },
-      });
+  builder.hook("GraphQLObjectType:fields", (fields, build, context) => {
+    const { inflection } = build;
+    const {
+      scope: { isPgGISGeographyType, pgGISType, pgGISSubtypeDetails },
+    } = context;
+    if (!isPgGISGeographyType || pgGISSubtypeDetails.subtype !== 1) {
+      return fields;
     }
-  );
+    const {
+      extend,
+      graphql: { GraphQLNonNull, GraphQLFloat },
+    } = build;
+    const xFieldName = inflection.gisXFieldName(
+      pgGISType,
+      pgGISSubtypeDetails.srid
+    );
+    const yFieldName = inflection.gisYFieldName(
+      pgGISType,
+      pgGISSubtypeDetails.srid
+    );
+    return extend(fields, {
+      [xFieldName]: {
+        type: new GraphQLNonNull(GraphQLFloat),
+        resolve(data: any) {
+          return data.__geojson.coordinates[0];
+        },
+      },
+      [yFieldName]: {
+        type: new GraphQLNonNull(GraphQLFloat),
+        resolve(data: any) {
+          return data.__geojson.coordinates[1];
+        },
+      },
+    });
+  });
 };
 export default plugin;
