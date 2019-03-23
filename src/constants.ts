c6af602f55e2839aedbc6ff3014f0589d646f2b2
Matt Bretl <mpbretl@gmail.com>
2019-03-23 04:11:58 +0000

    construct unique GraphQL types per  subtype/z/m/srid; register by typmod

--- src/constants.ts
+++ src/constants.ts
@@ -8,7 +8,15 @@ export const SUBTYPE_STRING_BY_SUBTYPE = {
   7: "geometry-collection",
 };
 
-export const GIS_SUBTYPE = {
+export const GIS_SUBTYPE: {
+  Point: 1;
+  LineString: 2;
+  Polygon: 3;
+  MultiPoint: 4;
+  MultiLineString: 5;
+  MultiPolygon: 6;
+  GeometryCollection: 7;
+} = {
   Point: 1,
   LineString: 2,
   Polygon: 3,
