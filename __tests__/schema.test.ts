f7f11831d2bd18629913862f6a1ce9265beb2714
Matt Bretl <mpbretl@gmail.com>
2019-03-27 10:51:28 +0000

    remove type assertion

--- __tests__/schema.test.ts
+++ __tests__/schema.test.ts
@@ -10,12 +10,6 @@ const options = {
 
 test("prints a schema with this plugin", () =>
   withPgClient(async (client: pg.PoolClient) => {
-    const schema = await createPostGraphileSchema(
-      // TODO: remove the client type assertion
-      // once postgraphile-core is fixed
-      (client as unknown) as pg.Client,
-      schemas,
-      options
-    );
+    const schema = await createPostGraphileSchema(client, schemas, options);
     expect(printSchemaOrdered(schema)).toMatchSnapshot();
   }));
