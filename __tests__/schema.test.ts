0ccde26943354e4a86c870bd5263faa7c6503814
Felix Zedén Yverås <felix@fzy.se>
2024-06-04 16:39:00 +0000

    Remove dependency on `postgraphile-core`
    `postgraphile-core` is no more according to documentation:
    https://postgraphile.org/postgraphile/next/migrating-from-v4/#postgraphile-core

--- __tests__/schema.test.ts
+++ __tests__/schema.test.ts
@@ -1,16 +1,22 @@
 import * as pg from "pg";
-import { withPgClient } from "./helpers";
-import { createPostGraphileSchema } from "postgraphile-core";
+import { withPgPool } from "./helpers";
 import PostgisPlugin from "../src/index";
 import { lexicographicSortSchema } from "graphql";
+import { makeSchema } from "graphile-build";
+import { makePgService } from "postgraphile/adaptors/pg";
 
 const schemas = ["graphile_postgis"];
-const options = {
-  appendPlugins: [PostgisPlugin],
-};
 
 test("prints a schema with this plugin", () =>
-  withPgClient(async (client: pg.PoolClient) => {
-    const gqlSchema = await createPostGraphileSchema(client, schemas, options);
+  withPgPool(async (pool: pg.Pool) => {
+    const gqlSchema = await makeSchema({
+      extends: [PostgisPlugin],
+      pgServices: [
+        makePgService({
+          pool,
+          schemas,
+        }),
+      ],
+    }).then(it => it.schema);
     expect(lexicographicSortSchema(gqlSchema)).toMatchSnapshot();
   }));
